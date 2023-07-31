dbcb0dc60f3b791062b03a70f6e9b40294788354
TheNewPadawan <lambertofulgione1@gmail.com>
2023-07-31 19:51:52 +0200

    Tested function adopt

--- src/main/java/io/thenewpadawan/example/vetsandpets/Main.java
+++ src/main/java/io/thenewpadawan/example/vetsandpets/Main.java
@@ -12,6 +12,15 @@ public class Main {
 		Person p2 = new Vet("lamb", "fulg");
 		Vet v1 = new Vet("marco", "rai");
 		Pet pet1 = new Pet("kaila", "labrador", new Date());
+		Pet pet2 = new Pet("Alan", "pastore scozzese", new Date());
 		System.out.println("2 People, 1 Vet and 1 Pet created");
+		//1) Ti do una persona
+		//2) Ti do un animale
+		//3) Fa si che la persona adotti l'animale
+		//nota bene: la relazione deve essere persistente
+		p1.adopt(pet1);
+		System.out.println("Che bel cucciolo, come si chiama? " + p1.getPet().getName());
+		p1.adopt(pet2);
+		System.out.println("Che bel cucciolo, come si chiama? " + p1.getPet().getName());
 	}
 }
