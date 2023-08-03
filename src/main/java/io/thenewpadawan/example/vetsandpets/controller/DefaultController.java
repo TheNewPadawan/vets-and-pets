28b57b2e4dcbab93fdcf596bb8758548f1bdd940
TheNewPadawan <lambertofulgione1@gmail.com>
2023-08-03 19:45:20 +0200

    Crud interface created for controllers

---
+++ src/main/java/io/thenewpadawan/example/vetsandpets/controller/DefaultController.java
@@ -0,0 +1,8 @@
+package io.thenewpadawan.example.vetsandpets.controller;
+
+public interface DefaultController {
+	public void create();
+	public void retrieve();
+	public void update();
+	public void delete();
+}
