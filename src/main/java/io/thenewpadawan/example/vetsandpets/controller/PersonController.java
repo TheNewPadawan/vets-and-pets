67a2e0ba3fe1074cd0583b5f7a727de40caa9a83
TheNewPadawan <lambertofulgione1@gmail.com>
2023-08-03 19:51:56 +0200

    PersonController created

---
+++ src/main/java/io/thenewpadawan/example/vetsandpets/controller/PersonController.java
@@ -0,0 +1,29 @@
+package io.thenewpadawan.example.vetsandpets.controller;
+
+public class PersonController implements DefaultController {
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void retrieve() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
